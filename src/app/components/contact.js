6dacf6e743e32169495cbbeb2abdc7c1bbcf52f2
Freddy Amin Zapata <83481744+goku673@users.noreply.github.com>
2025-01-13 23:51:39 -0400

    up date component contact

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -8,7 +8,7 @@ import { variants } from "./ui/badge";
     const Contact = () => (
             <Card>
                 <CardContent className="p-6">
-                    <h2 className="text-3xl font-extrabold mb-6 text-gray-800">Conectemos</h2>
+                    <h2 className="text-3xl font-extrabold mb-6 text-gray-800">Contact me</h2>
                     <div className="flex flex-col space-y-4">
                     {itemsContact.map(item => (
                         <Button
